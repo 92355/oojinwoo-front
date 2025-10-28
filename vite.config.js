948647173efa2bdf7b60a6cd37774e36289397f4
92355 <qpqp9235@gmail.com>
2025-10-29 08:24:16 +0900

    build3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ⚙️ GitHub Pages용 경로 설정
 export default defineConfig({
   plugins: [react()],
+  base: '/oojinwoo-front/', // GitHub repo 이름과 동일해야 함
 })
